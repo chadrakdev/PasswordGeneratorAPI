fd4b440b482d980582a636dc1e59ccf89e41b7bf
chadrakdev <chadrak@outlook.com>
2025-01-04 16:54:13 +0000

    Allow for nullable Response Criteria in case of error

--- Models/PasswordResponseData.cs
+++ Models/PasswordResponseData.cs
@@ -2,6 +2,6 @@ namespace PasswordGeneratorAPI.Models;
 
 public class PasswordResponseData
 {
-    public string Response { get; set; }
-    public PasswordCriteria Criteria { get; set; }
+    public string? Response { get; set; }
+    public PasswordCriteria? Criteria { get; set; }
 }
