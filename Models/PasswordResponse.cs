b7b61154412567bff8f812b4dafadfb02cbe6d13
chadrakdev <chadrak@outlook.com>
2025-01-04 16:54:59 +0000

    Allow for nullable data in case of error and add error field

--- Models/PasswordResponse.cs
+++ Models/PasswordResponse.cs
@@ -3,5 +3,7 @@ namespace PasswordGeneratorAPI.Models;
 public class PasswordResponse
 {
     public int Status { get; set; }
-    public PasswordResponseData Data { get; set; }
+    public PasswordResponseData? Data { get; set; }
+    
+    public string? Error { get; set; }
 }
